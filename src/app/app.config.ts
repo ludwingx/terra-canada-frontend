ebdea68654bd9e99ad053056699b760c926966f2
ludwingx <ludwingytx@gmail.com>
2026-01-31 10:05:58 -0400

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
@@ -15,7 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), 
     provideHttpClient(withFetch(), withInterceptors([authInterceptor, apiDebugInterceptor])),
-    provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
     providePrimeNG({ 
         theme: {
