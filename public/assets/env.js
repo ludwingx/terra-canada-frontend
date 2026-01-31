d03e95497fd2e846fca075885bd89691b6d1c3c5
ludwingx <ludwingytx@gmail.com>
2026-01-31 03:49:29 -0400

    update

--- public/assets/env.js
+++ public/assets/env.js
@@ -1,4 +1,4 @@
 window.__env = window.__env || {};
 window.__env = {
-  API_BASE_URL: 'https://terra-canada-control-pagos.vamw1k.easypanel.host/api/v1'
+  API_BASE_URL: "https://terra-canada-control-pagos.vamw1k.easypanel.host/api/v1"
 };
