80a6af19d178f2091fd22bc848f11250ec14f08f
ludwingx <ludwingytx@gmail.com>
2026-01-31 03:11:20 -0400

    update

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,10 +1,16 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
   const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  if (!isPlatformBrowser(platformId)) {
+    return true;
+  }
 
   if (auth.isAuthenticated()) {
     return true;
