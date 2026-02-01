e1b11794030ad7679359b4bceb3cf547ceb9ff16
ludwingx <ludwingytx@gmail.com>
2026-02-01 12:15:10 -0400

    Remove sensitive env files from tracking

--- public/assets/env.js
+++
@@ -1,5 +0,0 @@
-window.__env = window.__env || {};
-window.__env = {
-  API_BASE_URL: "https://terra-canada-servicio-api.vamw1k.easypanel.host/api/v1",
-  DEBUG_API: false
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://terra-canada-servicio-api.vamw1k.easypanel.host/api/v1' // Updated to the new environment
-};
